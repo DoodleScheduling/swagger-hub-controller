f76dd0265698eb77d3dc1086b080859b9cee6c0c
Raffael Sahli <raffael.sahli@doodle.com>
2026-08-27 11:26:22 +0200

    fix: format eventf

--- internal/controllers/swaggerspecification_controller.go
+++ internal/controllers/swaggerspecification_controller.go
@@ -134,7 +134,7 @@ func (r *SwaggerSpecificationReconciler) Reconcile(ctx context.Context, req ctrl
 	if err != nil {
 		logger.Error(err, "reconcile error occurred")
 		specification = infrav1beta1.SwaggerSpecificationReady(specification, metav1.ConditionFalse, "ReconciliationFailed", err.Error())
-		r.Recorder.Eventf(&specification, nil, "Normal", "error", "", "%s", err.Error())
+		r.Recorder.Eventf(&specification, nil, corev1.EventTypeNormal, "Error", "Reconcile", "failed to reconcile: %s", err.Error())
 	}
 
 	// Update status after reconciliation.
